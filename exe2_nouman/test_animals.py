959ec0516fcd2c03adbe3a15c449335c244cebfb
Nouman Ahmad <noumanahmad@emp-84-145.eduroam.uu.se>
2023-03-07 13:36:59 +0100

    Add all my files

---
+++ exe2_nouman/test_animals.py
@@ -0,0 +1,9 @@
+# Import classes from your brand new package
+from animals import Mammals 
+from animals import Birds
+# Create an object of Mammals class & call a method of it
+myMamma1 = Mammals ()
+myMamma1.printMembers ()
+# Create an object of Birds class & call a method of it
+myBird = Birds ()
+myBird.printMembers()
